a5491503338db4f2540279918a66d501e48b50db
zdenka honzalova <zpalffyova@gmail.com>
2026-04-20 23:12:30 +0200

    Multiple changes on pages - home, about, blog

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
